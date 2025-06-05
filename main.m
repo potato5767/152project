b1757b677e0072b2ae5991797ad1b0e3e4feda71
potato5767 <48464781+potato5767@users.noreply.github.com>
2025-06-05 15:41:39 -0700

    Update main.m
    fixed original vid output

--- main.m
+++ main.m
@@ -19,7 +19,7 @@ outputE = enhanceE(grayFrame);
 %subplot initialization
 imageHandles = gobjects(1, 6);
 titles = {"Original", "Method A", "Method B", "Method C", "Method D", "Method E"};
-images = {grayFrame, outputA, outputB, outputC, outputD, outputE};
+images = {frame, outputA, outputB, outputC, outputD, outputE};
 for i = 1:6
     subplot(2, 3, i);
     imageHandles(i) = imshow(images{i});
@@ -37,7 +37,7 @@ while hasFrame(vidHandle)
     outputD = enhanceD(grayFrame);
     outputE = enhanceE(grayFrame);
     %updating handles
-    updatedImages = {grayFrame, outputA, outputB, outputC, outputD, outputE};
+    updatedImages = {frame, outputA, outputB, outputC, outputD, outputE};
     %refreshing frames
     for i = 1:6
         set(imageHandles(i), 'CData', updatedImages{i});
